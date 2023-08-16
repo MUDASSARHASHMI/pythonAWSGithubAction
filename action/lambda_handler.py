510c40ae60fc052726c648337b7f6b534d87297f
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2023-08-16 19:01:26 -0400

    initial

---
+++ action/lambda_handler.py
@@ -0,0 +1,14 @@
+def lambda_handler(event, context):
+    """
+    Lambda function wrapper
+    Args:
+        event: trigger event dict
+        context: lambda methods and properties
+    Returns:
+        string: greeting response
+    """
+    print("Starting functions\n.....................................")
+    if event("input") == "Hello":
+        return "World"
+    else:
+        raise ValueError("Input must be Hello")
