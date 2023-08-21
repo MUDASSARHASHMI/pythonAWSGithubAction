ff7d30305762947cde361ddf44ccef01d04a7c00
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2023-08-21 19:01:50 -0400

    changed handler

--- action/lambda_handler.py
+++ action/lambda_handler.py
@@ -1,9 +1,20 @@
 def lambda_handler(event, context):
-    print("Starting functions\n.....................................")
+    """Lambda function wrapper
+    Args:
+        event: trigger event dict
+        context: lambda methods and properties
+    Returns:
+        string: greeting response
+    """
+    print('Starting functions\n---------------------------------------------')
+
     if event["input"] == "Hello":
         return "World"
-    if event["input"] == "Hi":
-        return "Hi There"
-    else:
-        raise
 
+    if event["input"] == "Hi":
+
+        return "Hi There"
+
+    else:
+
+        raise
