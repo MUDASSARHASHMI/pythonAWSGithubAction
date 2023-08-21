ddb45b6995ba7ccc72a1e2139ffccd9e6af876e3
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2023-08-21 02:39:11 -0400

    lambda role fixed

--- action/lambda_handler.py
+++ action/lambda_handler.py
@@ -1,18 +1,9 @@
 def lambda_handler(event, context):
-    """
-    Lambda function wrapper
-    Args:
-        event: trigger event dict
-        context: lambda methods and properties
-    Returns:
-        string: greeting response
-    Functions:
-        Added two test functions test-function and Matrix
-    """
     print("Starting functions\n.....................................")
     if event["input"] == "Hello":
         return "World"
     if event["input"] == "Hi":
         return "Hi There"
     else:
-        raise ValueError("Input must be Hello")
+        raise
+
